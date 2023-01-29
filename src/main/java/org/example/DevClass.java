413c0a46c23a8400b96f40cbf3e1547bb2b701f2
mdAmirGitMaster <barshi631@123>
2023-01-29 21:13:53 +0530

    Ya Ali....Git 2nd commit by amir 29Jan2023 17 13pm 2

---
+++ src/main/java/org/example/DevClass.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DevClass {
+
+    public static void main(String[] args) {
+        System.out.println("DevClass");
+        System.out.println("DevClass2");
+        System.out.println("DevClass2");
+
+
+
+    }
+}
