0e7ca0e8943f0aec1e70a2a5b49ac3b4f3e9c220
mdAmirGitMaster <barshi631@123>
2023-01-29 21:01:23 +0530

    Ya Ali....Git 2nd commit by amir 29Jan2023 17 13pm

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,6 +1,6 @@
 public class DevTestClass {
     public static void main(String[] args) {
-        System.out.println("DevTestClass");
-
+        System.out.println("DevTes amir tClass khan");
+        System.out.println("DevTestClass22222222222222");
     }
 }
